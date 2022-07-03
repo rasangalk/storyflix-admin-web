0115067c8880ca2354a8906edbcfc3a60e873bcd
Rasanga Lakshith <64822421+rasangalk@users.noreply.github.com>
2022-07-03 16:53:03 +0530

    routes and router initialized

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import Test from "./components/Test";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Test />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
