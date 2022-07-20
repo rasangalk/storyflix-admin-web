952516ccd6172acf3ffc9e354525dea7ace3bfe1
Rasanga Lakshith <64822421+rasangalk@users.noreply.github.com>
2022-07-20 19:44:53 +0530

    comments added to private routes file

--- src/components/HOC/privateaRoute.js
+++ src/components/HOC/privateaRoute.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Outlet, Navigate } from "react-router";
 
+/**
+ * This function handles the private routings
+ */
 const PrivateWrapper = () => {
   const token = window.localStorage.getItem("token");
   return token ? <Outlet /> : <Navigate to="/login" replace />;
